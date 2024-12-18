0fe83c16d4185cb3ede796f7b5f08aba1b3aa3b8
Tonyf6 <149448421+Tonyf6@users.noreply.github.com>
2024-12-17 21:18:52 -0800

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+import Grid from "@/components/Grid";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 import Hero from "@/components/ui/Hero";
+import { FaHome } from "react-icons/fa";
 
 
 
@@ -7,7 +10,9 @@ export default function Home() {
    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 ">
     <div className="max-w-7xl w-full ">
       <h1 className="text-white-200">Hello, Portfolio</h1>
+      <FloatingNav navItems={[{name:"Home",link:'/',icon:<FaHome/>}]}/>
       <Hero/>
+      <Grid/>
     </div>
    </main>
   );
--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -1,32 +1,21 @@
 "use client";
 import React, { ReactNode } from "react";
-import { IconClipboard } from "@tabler/icons-react";
-import { cn } from "@/lib/utils";
+
 
 const MagicButton = ({
-  title,
-  icon,
-  position,
-  handleClick,
-  otherClasses,
+  title,icon,position,otherClasses
 }: {
-  title: string;
-  icon: ReactNode;
-  position: string;
-  handleClick?: () => void;
-  otherClasses?: string;
+  title:string;
+  icon:ReactNode;
+  position:string;
+//   handleClick?: () => void;
+  otherClasses?:string
 }) => {
   return (
-    <button className="shadow-[inset_0_0_0_2px_#616467] text-black px-12 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#405161] hover:text-white dark:text-neutral-200 transition duration-200">
-      <span
-        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg px-7 text-sm font-medium text-white gap-2 ${otherClasses}`}
-      >
-        {position === "left" && icon}
-        {title}
-        {position === "right" && icon}
-      </span>
-    </button>
+    // Button code
+    <button className=" w-full inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 md:w-60 md: mt-10">
+    <span className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg px-7 text-sm font-medium text-white gap-2 ${otherClasses}`}>{position === 'left' && icon}{title}{position === 'right' && icon}</span>
+  </button>
   );
 };
-
 export default MagicButton;
